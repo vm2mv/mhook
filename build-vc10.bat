cb463ae281377fffd8540b6091f240b370e38be7
artur.bulakaiev <bulakaiev.artur@apriorit.com>
2017-08-17 21:05:29 +0300

    [improvement] Old project generation system removed. CMake generation added.

--- build-vc10.bat
+++
@@ -1,4 +0,0 @@
-call mvn -f ./pom/x86-windows-static-mt-msvc10-release/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x86-windows-static-mt-msvc10-debug/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x64-windows-static-mt-msvc10-release/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x64-windows-static-mt-msvc10-debug/pom.xml deploy || goto :EOF
--- build-vc11.bat
+++
@@ -1,4 +0,0 @@
-call mvn -f ./pom/x86-windows-static-mt-msvc11-release/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x86-windows-static-mt-msvc11-debug/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x64-windows-static-mt-msvc11-release/pom.xml deploy || goto :EOF
-call mvn -f ./pom/x64-windows-static-mt-msvc11-debug/pom.xml deploy || goto :EOF

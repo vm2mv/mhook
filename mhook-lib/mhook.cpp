6911b577a4c71aa7c075a96a39d10892c8f00b40
Vladimir Vasilevskiy <vladimir.vasilevskiy@dev.local>
2019-06-05 16:33:53 +0300

    changed int3 to hlt overwrite byte

--- mhook-lib/mhook.cpp
+++ mhook-lib/mhook.cpp
@@ -448,7 +448,7 @@ static PBYTE AntiDetoursFill(PBYTE pbCode, DWORD externalBytes)
 {
     while (externalBytes-- > 0)
     {
-        *(pbCode++) = 0xCC;
+        *(pbCode++) = 0xF4;
     }
     return pbCode;
 }

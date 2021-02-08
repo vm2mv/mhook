938889e19dd6cac74ac2a2ea03df4a04b663e4b1
Sergey Podobry <sergius@apriorit.com>
2021-02-08 19:15:27 +0200

    Fix wrong assert

--- mhook-lib/mhook.c
+++ mhook-lib/mhook.c
@@ -836,7 +836,7 @@ static BOOL GetCurrentProcessSnapshot(PVOID* snapshot, PSYSTEM_PROCESS_INFORMATI
 
     // this never returns NULL as the current process id is always present in the processes snapshot
     *procInfo = FindProcess(*snapshot, GetCurrentProcessId());
-    assert(procInfo);
+    assert(*procInfo);
 
     return TRUE;
 }
